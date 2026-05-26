3fb162fda69c9ac8199ccc31df9639e633b80c08
gabriella Piovezan <gabymarqueserosa@gmail.com>
2026-05-26 14:37:07 -0300

    criando menu

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("---Buscapé produtos Naturais---");
+        System.out.println("---Bem-vindo ao sistema!---");
+        System.out.println("Como podemos ajudar?");
+        System.out.println("---Menu do Buscapé---");
+        System.out.println("-1 Cadastro de Clientes: ");
+        System.out.println("-2 Cadastro de Produtos: ");
+        System.out.println("-3 Listar todos os Clientes: ");
+        System.out.println("-4 Listar todos os Produtos: ");
+    }
+}
